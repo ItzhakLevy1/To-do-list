978fdac8b0b2b8694f207ab3a4dab8dda5122732
איציק לוי <itzhaklevydev@gmail.com>
2022-02-25 10:46:21 +0200

    Add gitignore, procfile and update ports, a second time

--- app.js
+++ app.js
@@ -134,12 +134,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;  // Allowing our program to run both on our local system as well as on heroku.
-if (port == null || port == "") {   // Check if a port from heroku exists, if not than run the program localy on port 3000. 
+let port = process.env.PORT;  // Allowing to listen both to the heroku port "process.env.PORT" and to our local port 3000.
+if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
 
-app.listen(port, function() {   // Referencing "port" that could be either heroku's port "process.env.PORT" or our local port 3000.
-  console.log("Server has started, success.");
+app.listen(port, function() {
+  console.log("Server has started successfully.");
 });
